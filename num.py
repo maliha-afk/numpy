78edbd8d58ac67ff22f76a489175018acf5b56bc
maliha-afk <24amaliha@gmail.com>
2024-12-11 22:18:22 +0600

    Add files via upload

---
+++ num.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+arr=np.array([10,2,5,4,6])
+#arr2=np.array([6,7,8,9,10])
+
+
+#print(arr+arr2)
+#print(arr-arr2)
+#print(arr/arr2)
+#print(arr*arr2)
+
+#print(np.sum(arr))
+#print(np.max(arr))
+#print(np.median(arr2))
+#print(np.mean(arr))
+#print(np.min(arr))
+
+#mat1=np.array([[1,2,3],[4,5,6]])
+#print(mat1)
+
+#mat2=np.arange(1,10).reshape(3,3)
+#print(mat2)
+
+#print(np.sort(arr))
+
+print(np.where(arr2 == 2))
+print(np.isin(30,arr))
